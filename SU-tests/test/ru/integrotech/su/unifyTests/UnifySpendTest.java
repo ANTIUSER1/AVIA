debb91c990ae8e9aab4fa8970d96f865347b3d81
Agorbunov <agorbunov@integrotechnologies.ru>
2019-12-05 15:59:52 +0300

    unufy spend tests report headers

--- SU-tests/test/ru/integrotech/su/unifyTests/UnifySpendTest.java
+++ SU-tests/test/ru/integrotech/su/unifyTests/UnifySpendTest.java
@@ -44,12 +44,14 @@ public class UnifySpendTest extends UnifyBaseTest {
         jsonElement = this.common.getLoader().loadJson(pathToCaseFolder, EXPECTED_RESPONSE_FILE_NAME);
         List<SpendRoute> expectedSpendRoutes = this.common.getTestsCache().loadSpendRoutes(jsonElement);
 
-        String testReport = this.compareSpendRoute(expectedSpendRoutes, actualSpendRoutes);
-        boolean result = testReport.contains(SUCCESS)
-                    && ( !testReport.contains(INCORRECT)
-                         &&!testReport.contains(NOT_FOUND)
-                         &&!testReport.contains(EXTRA));
+        String testHeader = this.buildReportHeader(spendInput);
+        String testBody = this.compareSpendRoute(expectedSpendRoutes, actualSpendRoutes);
+        boolean result = testBody.contains(SUCCESS)
+                    && ( !testBody.contains(INCORRECT)
+                         &&!testBody.contains(NOT_FOUND)
+                         &&!testBody.contains(EXTRA));
         printTestResults(result, actualSpendRoutes, pathToCaseFolder);
+        String testReport = String.format("%s%s", testHeader, testBody);
         printReport(testReport, pathToCaseFolder);
         return result;
     }
@@ -59,14 +61,76 @@ public class UnifySpendTest extends UnifyBaseTest {
         Assert.assertTrue(executeTest(ROOT_TEST_DIRECTORY_PATH));
     }
 
+    private String buildReportHeader(SpendInput spendInput) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(" ------------ SPEND TEST ------------\n");
+        builder.append("|  Parameters:                       |\n");
+
+        //origin block
+        builder.append("|------------------------------------|\n");
+        builder.append("|  Origin:                           |\n");
+
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                                            "location type:",
+                                            spendInput.getOrigin().getLocationType()));
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                                            "location code:",
+                                            spendInput.getOrigin().getLocationCode()));
+
+        //destination block
+        builder.append("|------------------------------------|\n");
+        builder.append("|  Destination:                      |\n");
+
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                "location type:",
+                spendInput.getDestination().getLocationType()));
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                "location code:",
+                spendInput.getDestination().getLocationCode()));
+
+        //miles interval block
+        builder.append("|------------------------------------|\n");
+        builder.append("|  Miles interval:                   |\n");
+
+        builder.append(String.format("|  %-20s %,-10d   |\n",
+                "miles min:",
+                spendInput.getMilesInterval().getMilesMin()));
+        builder.append(String.format("|  %-20s %,-10d   |\n",
+                "miles max:",
+                spendInput.getMilesInterval().getMilesMax()));
+
+        //other values block
+        builder.append("|------------------------------------|\n");
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                "Is only afl:", spendInput.isOnlyAfl()));
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                "Is round trip:", spendInput.isRoundTrip()));
+        String classOfServiceName = "null";
+        if (spendInput.getClassOfService() != null) {
+            classOfServiceName = spendInput.getClassOfService().getClassOfServiceName();
+        }
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                "Class of service:", classOfServiceName));
+        builder.append(String.format("|  %-20s %-10s   |\n",
+                "Award type:", spendInput.getAwardType()));
+        builder.append(" ------------------------------------\n");
+        builder.append("\n");
+
+        return builder.toString();
+    }
+
+
+
     private String compareSpendRoute(List<SpendRoute> expected, List<SpendRoute> actual) {
         StringBuilder builder = new StringBuilder();
-        builder.append(String.format("%-23s     %s\n", " --- R O U T E ---", "RESULT"));
+        builder.append(String.format("   %-23s     %s\n", " --- R O U T E ---", "RESULT"));
         builder.append("\n");
-        this.sort(actual);
         this.sort(expected);
+        this.sort(actual);
         ArrayList<SpendRoute> actualArr = new ArrayList<>(actual);
         ArrayList<SpendRoute> expectedlArr = new ArrayList<>(expected);
+        int successCounter = 0;
+        int incorrectCounter = 0;
         Iterator<SpendRoute> expectedIterator = expectedlArr.iterator();
         expected: while (expectedIterator.hasNext()) {
             SpendRoute expectedRoute = expectedIterator.next();
@@ -75,13 +139,15 @@ public class UnifySpendTest extends UnifyBaseTest {
                 SpendRoute actualRoute = actualIterator.next();
                 if (this.isShallowEquals(expectedRoute, actualRoute)) {
                     if (this.isDeepEquals(expectedRoute, actualRoute)) {
-                        builder.append(String.format("%-25s     %s",
+                        builder.append(String.format("   %-25s     %s",
                                 this.getRoteCode(actualRoute),
                                 SUCCESS));
+                        successCounter++;
                     } else {
-                        builder.append(String.format("%-25s     %s",
+                        builder.append(String.format("   %-22s     %s",
                                 this.getRoteCode(actualRoute),
                                 INCORRECT));
+                        incorrectCounter++;
                     }
                     expectedIterator.remove();
                     actualIterator.remove();
@@ -91,19 +157,46 @@ public class UnifySpendTest extends UnifyBaseTest {
         }
 
         for (SpendRoute route : expectedlArr) {
-            builder.append(String.format("%-25s     %s",
+            builder.append(String.format("   %-22s     %s",
                     this.getRoteCode(route),
                     NOT_FOUND));
         }
 
         for (SpendRoute route : actualArr) {
-            builder.append(String.format("%-25s     %s",
+            builder.append(String.format("   %-23s     %s",
                     this.getRoteCode(route),
                     EXTRA));
         }
 
+        builder.append("\n");
+        builder.append(" ------------- RESULTS --------------\n");
+
+        if ((expectedlArr.size() == 0)
+            && (actualArr.size() == 0)
+            && incorrectCounter == 0){
+                builder.append("|            Tests is OK             |\n");
+        } else {
+            builder.append(String.format("|  %-20s %,10d   |\n",
+                    "expected routes:", expected.size()));
+            builder.append(String.format("|  %-20s %,10d   |\n",
+                    "actual routes:", actual.size()));
+
+            builder.append(String.format("|  %-20s %,10d   |\n",
+                    "OK:", successCounter));
+            builder.append(String.format("|  %-20s %,10d   |\n",
+                    "incorrect:", incorrectCounter));
+
+            builder.append(String.format("|  %-20s %,10d   |\n",
+                    "not found:", expectedlArr.size()));
+            builder.append(String.format("|  %-20s %,10d   |\n",
+                    "extra:", actualArr.size()));
+        }
+
+        builder.append(" ------------------------------------ \n");
+
         return builder.toString();
     }
+
 
     private String getRoteCode(SpendRoute route) {
         StringBuilder builder = new StringBuilder();
@@ -114,7 +207,7 @@ public class UnifySpendTest extends UnifyBaseTest {
             builder.append(String.format("%s  ", "   "));
         }
         builder.append(String.format("%s ", route.getDestination().getAirport().getAirportCode()));
-        builder.append(String.format("  %s", route.isAfl() ? "afl" : "scyteam"));
+        builder.append(String.format("  %s", route.isAfl() ? "  afl" : "scyteam"));
         return builder.toString();
     }
 
